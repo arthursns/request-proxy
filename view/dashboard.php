d41b94651de59f7a98efbcfcf17a70220d8de9b0
Arthur Silva <arthursns@gmail.com>
2021-07-20 09:17:31 -0300

    Organizações Diretórios

--- view/dashboard.php
+++ view/dashboard.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'connect.php';
-require_once 'protect.php';
+require_once '../model/connect.php';
+require_once '../protect.php';
 ?>
 
 <!DOCTYPE html>
@@ -17,7 +17,7 @@ require_once 'protect.php';
         <link href="css/styles.css" rel="stylesheet" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.js"></script>
-        <script type="text/javascript" src="js/script.js"></script>
+        <script type="text/javascript" src="../js/script.js"></script>
     </head>
     <body>
     <?php include 'sidebarTop.php';?>
---
+++ view/sidebarTop.php
@@ -0,0 +1,12 @@
+<div class="d-flex" id="wrapper">
+            <!-- Page content wrapper-->
+            <div id="page-content-wrapper">
+                <!-- Top navigation-->
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="dashboard.php">Liberação Firewall</a>
+                    <div class="container-fluid">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                    </div>
+                </nav>
+                <!-- Page content-->
+                <div class="container-fluid">
